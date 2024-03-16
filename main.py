6f8342b62a837151006a5b85d87f44800a9f3f8e
Prashanta mondal <iloveprashanta@gmail.com>
2024-03-16 22:35:25 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+import requests
+import json
+
+url = "http://localhost:11434/api/generate"
+
+headers = {
+    'Content-Type': 'application/json',
+}
+
+data = {
+    "model": "llama2",
+    "stream": False,
+    "prompt": "Why is the sky blue?"
+}
+
+response = requests.post(url, headers=headers, data=json.dumps(data))
+if response.status_code == 200:
+    response_text = response.text
+    data = json.loads(response_text)
+    actual_response = data["response"]
+    print(actual_response)
+else:
+    print("error", response.status_code, response.text)
